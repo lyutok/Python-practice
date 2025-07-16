6779d024cd61754fc19cbe48aa9b2a0609de4232
lyutok <48644325+lyutok@users.noreply.github.com>
2025-07-16 15:52:07 +0300

    Edit solution

--- ArrayMerge/ArrayMerge.py
+++ ArrayMerge/ArrayMerge.py
@@ -30,13 +30,25 @@ Note that because m = 0, there are no elements in nums1. The 0 is only there to 
 """
 
 def merge(nums1, m, nums2, n):
+    i = m - 1  
+    j = n - 1  
+    k = m + n - 1  
+   
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
 
-    for i in range(len(nums1)):
-        if nums1[i] == 0:
-            n -= 1
-            nums1[i] = nums2[n]
+    # for i in range(len(nums1)):
+    #     if nums1[i] == 0:
+    #         n -= 1
+    #         nums1[i] = nums2[n]
             
-    nums1.sort()
+    # nums1.sort()
 
 # Example usage
 nums1 = [1,2,3,0,0,0]
