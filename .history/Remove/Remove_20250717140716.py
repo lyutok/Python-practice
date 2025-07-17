332a2fb15908bfe140de55fd1be08cb40e5f89ea
lyutok <48644325+lyutok@users.noreply.github.com>
2025-07-17 16:39:42 +0300

    Resolve remove

---
+++ .history/Remove/Remove_20250717140716.py
@@ -0,0 +1,30 @@
+"""
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+The remaining elements of nums are not important as well as the size of nums.
+
+Return k.
+
+"""
+nums = [3,2,2,3]
+val = 3
+k = 0
+n = 0
+
+total = len(nums)
+
+for i in range(total):
+    # for j in range(total-1, 0, -1):
+    if nums[i] == val:
+        nums[i] = nums[i+1]
+        nums[i+1] = nums[i]
+        n += 1
+
+                
+k = total - n
+print(k)
+print(nums)
---
+++ .history/Remove/Remove_20250717150405.py
@@ -0,0 +1,34 @@
+"""
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+The remaining elements of nums are not important as well as the size of nums.
+
+Return k.
+
+"""
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+# k = 0
+
+total = len(nums)
+nums.remove(val)
+
+# for i in range(total-1):
+#     if nums[i] == val:
+#         nums.pop(i)
+
+# for num in nums:
+#     if num != val:
+#         k += 1
+
+# for i in range(total-1):
+#     if nums[i] == val:
+#         nums[i] = nums[i+1]
+#         nums[i+1] = val
+
+print(k)
+print(nums)
