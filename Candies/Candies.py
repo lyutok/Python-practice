82acf57517679bfffddbae872b7aebc2fcec3a9a
lyutok <48644325+lyutok@users.noreply.github.com>
2025-07-19 15:22:22 +0300

    Resolution

--- Candies/Candies.py
+++ Candies/Candies.py
@@ -8,10 +8,24 @@ Children with a higher rating get more candies than their neighbors.
 Return the minimum number of candies you need to have to distribute the candies to the children.
 """
 
-ratings = [1,0,2]
+ratings = [1,3,2,2,1]
+# Each child must have at least one candy.
 candy = [1] * len(ratings)
-
-for i in len(ratings):
-    if i 
 print(candy)
+
+for i in range(1, len(ratings)):
+    if ratings[i] > ratings[i - 1]:
+        candy[i] = candy[i - 1] + 1
+
+print(candy)
+
+for i in range(len(ratings) - 2, -1, -1):
+    if ratings[i] > ratings[i + 1]:
+        candy[i] = max(candy[i], candy[i + 1] + 1)
+
+print(candy)
+# Calculate the total number of candies needed
+total_candies = sum(candy)
+print(total_candies)  
+
 
